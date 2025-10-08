6bd1ce28c74a35ffa2c8b30c69616119aa2b483e
ritikaaaguptaaa <ritika@mail.hybrowlabs.com>
2025-10-08 19:56:13 +0530

    sub department doctype shifted to holiday tribe

--- holidaytribe_hrms/holidaytribe_hrms/doctype/sub_department/sub_department.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, BluePhoenix and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Sub Department", {
-// 	refresh(frm) {
-
-// 	},
-// });
